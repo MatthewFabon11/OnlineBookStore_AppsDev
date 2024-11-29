f10f1c30050c4c6bc3086c23c0970c17c0954812
MatthewFabon11 <matthewfabon11@gmail.com>
2024-11-29 22:06:12 +0800

    Add files via upload

---
+++ BOOK UP/OnlineBookStore_AppsDev/AdminPanel.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace OnlineBookStore
+{
+    partial class AdminPanel
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            GenreCmbBox = new ComboBox();
+            label11 = new Label();
+            DeleteTxtBox = new TextBox();
+            DeleteBookLbl = new Label();
+            ResultTxtBox = new TextBox();
+            AcadCmbBox = new ComboBox();
+            AcademicsLbl = new Label();
+            NonFictionCmbBox = new ComboBox();
+            NonFinctionLbl = new Label();
+            FictionCmbBox = new ComboBox();
+            FictionLbl = new Label();
+            NextBttn = new Button();
+            BackBttn = new Button();
+            DeleteBttn = new Button();
+            UpdateBttn = new Button();
+            AddBttn = new Button();
+            PublishTxtBox = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            ISBNTxtBox = new TextBox();
+            label4 = new Label();
+            AuthorTxtBox = new TextBox();
+            label3 = new Label();
+            TitleTxtBox = new TextBox();
+            label5 = new Label();
+            AdminLbl = new Label();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // GenreCmbBox
+            // 
+            GenreCmbBox.FormattingEnabled = true;
+            GenreCmbBox.Items.AddRange(new object[] { "Fiction", "Non-Fiction", "Academic" });
+            GenreCmbBox.Location = new Point(118, 298);
+            GenreCmbBox.Name = "GenreCmbBox";
+            GenreCmbBox.Size = new Size(312, 28);
+            GenreCmbBox.TabIndex = 57;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(58, 419);
+            label11.Name = "label11";
+            label11.Size = new Size(55, 20);
+            label11.TabIndex = 56;
+            label11.Text = "Results";
+            // 
+            // DeleteTxtBox
+            // 
+            DeleteTxtBox.Location = new Point(592, 399);
+            DeleteTxtBox.Name = "DeleteTxtBox";
+            DeleteTxtBox.Size = new Size(203, 27);
+            DeleteTxtBox.TabIndex = 55;
+            // 
+            // DeleteBookLbl
+            // 
+            DeleteBookLbl.AutoSize = true;
+            DeleteBookLbl.Location = new Point(483, 402);
+            DeleteBookLbl.Name = "DeleteBookLbl";
+            DeleteBookLbl.Size = new Size(105, 20);
+            DeleteBookLbl.TabIndex = 54;
+            DeleteBookLbl.Text = "Delete A book";
+            // 
+            // ResultTxtBox
+            // 
+            ResultTxtBox.Location = new Point(58, 454);
+            ResultTxtBox.Name = "ResultTxtBox";
+            ResultTxtBox.Size = new Size(203, 27);
+            ResultTxtBox.TabIndex = 53;
+            // 
+            // AcadCmbBox
+            // 
+            AcadCmbBox.FormattingEnabled = true;
+            AcadCmbBox.Location = new Point(483, 341);
+            AcadCmbBox.Name = "AcadCmbBox";
+            AcadCmbBox.Size = new Size(312, 28);
+            AcadCmbBox.TabIndex = 52;
+            // 
+            // AcademicsLbl
+            // 
+            AcademicsLbl.AutoSize = true;
+            AcademicsLbl.Location = new Point(483, 306);
+            AcademicsLbl.Name = "AcademicsLbl";
+            AcademicsLbl.Size = new Size(81, 20);
+            AcademicsLbl.TabIndex = 51;
+            AcademicsLbl.Text = "Academics";
+            // 
+            // NonFictionCmbBox
+            // 
+            NonFictionCmbBox.FormattingEnabled = true;
+            NonFictionCmbBox.Location = new Point(483, 241);
+            NonFictionCmbBox.Name = "NonFictionCmbBox";
+            NonFictionCmbBox.Size = new Size(312, 28);
+            NonFictionCmbBox.TabIndex = 50;
+            // 
+            // NonFinctionLbl
+            // 
+            NonFinctionLbl.AutoSize = true;
+            NonFinctionLbl.Location = new Point(483, 207);
+            NonFinctionLbl.Name = "NonFinctionLbl";
+            NonFinctionLbl.Size = new Size(87, 20);
+            NonFinctionLbl.TabIndex = 49;
+            NonFinctionLbl.Text = "Non-Fiction";
+            // 
+            // FictionCmbBox
+            // 
+            FictionCmbBox.FormattingEnabled = true;
+            FictionCmbBox.Location = new Point(483, 146);
+            FictionCmbBox.Name = "FictionCmbBox";
+            FictionCmbBox.Size = new Size(312, 28);
+            FictionCmbBox.TabIndex = 48;
+            // 
+            // FictionLbl
+            // 
+            FictionLbl.AutoSize = true;
+            FictionLbl.Location = new Point(483, 110);
+            FictionLbl.Name = "FictionLbl";
+            FictionLbl.Size = new Size(53, 20);
+            FictionLbl.TabIndex = 32;
+            FictionLbl.Text = "Fiction";
+            // 
+            // NextBttn
+            // 
+            NextBttn.Location = new Point(766, 519);
+            NextBttn.Name = "NextBttn";
+            NextBttn.Size = new Size(94, 29);
+            NextBttn.TabIndex = 47;
+            NextBttn.Text = "Next";
+            NextBttn.UseVisualStyleBackColor = true;
+            // 
+            // BackBttn
+            // 
+            BackBttn.Location = new Point(642, 519);
+            BackBttn.Name = "BackBttn";
+            BackBttn.Size = new Size(94, 29);
+            BackBttn.TabIndex = 46;
+            BackBttn.Text = "Back";
+            BackBttn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBttn
+            // 
+            DeleteBttn.Location = new Point(713, 454);
+            DeleteBttn.Name = "DeleteBttn";
+            DeleteBttn.Size = new Size(94, 29);
+            DeleteBttn.TabIndex = 45;
+            DeleteBttn.Text = "Delete";
+            DeleteBttn.UseVisualStyleBackColor = true;
+            // 
+            // UpdateBttn
+            // 
+            UpdateBttn.Location = new Point(602, 454);
+            UpdateBttn.Name = "UpdateBttn";
+            UpdateBttn.Size = new Size(94, 29);
+            UpdateBttn.TabIndex = 44;
+            UpdateBttn.Text = "Update";
+            UpdateBttn.UseVisualStyleBackColor = true;
+            // 
+            // AddBttn
+            // 
+            AddBttn.Location = new Point(483, 454);
+            AddBttn.Name = "AddBttn";
+            AddBttn.Size = new Size(94, 29);
+            AddBttn.TabIndex = 43;
+            AddBttn.Text = "Add";
+            AddBttn.UseVisualStyleBackColor = true;
+            // 
+            // PublishTxtBox
+            // 
+            PublishTxtBox.Location = new Point(183, 360);
+            PublishTxtBox.Name = "PublishTxtBox";
+            PublishTxtBox.Size = new Size(247, 27);
+            PublishTxtBox.TabIndex = 42;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(58, 363);
+            label7.Name = "label7";
+            label7.Size = new Size(119, 20);
+            label7.TabIndex = 41;
+            label7.Text = "Publication Date";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(58, 306);
+            label6.Name = "label6";
+            label6.Size = new Size(48, 20);
+            label6.TabIndex = 40;
+            label6.Text = "Genre";
+            // 
+            // ISBNTxtBox
+            // 
+            ISBNTxtBox.Location = new Point(118, 242);
+            ISBNTxtBox.Name = "ISBNTxtBox";
+            ISBNTxtBox.Size = new Size(312, 27);
+            ISBNTxtBox.TabIndex = 39;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(58, 249);
+            label4.Name = "label4";
+            label4.Size = new Size(41, 20);
+            label4.TabIndex = 38;
+            label4.Text = "ISBN";
+            // 
+            // AuthorTxtBox
+            // 
+            AuthorTxtBox.Location = new Point(118, 176);
+            AuthorTxtBox.Name = "AuthorTxtBox";
+            AuthorTxtBox.Size = new Size(312, 27);
+            AuthorTxtBox.TabIndex = 37;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(58, 179);
+            label3.Name = "label3";
+            label3.Size = new Size(54, 20);
+            label3.TabIndex = 36;
+            label3.Text = "Author";
+            // 
+            // TitleTxtBox
+            // 
+            TitleTxtBox.Location = new Point(118, 110);
+            TitleTxtBox.Name = "TitleTxtBox";
+            TitleTxtBox.Size = new Size(312, 27);
+            TitleTxtBox.TabIndex = 35;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(58, 110);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 20);
+            label5.TabIndex = 34;
+            label5.Text = "Title";
+            // 
+            // AdminLbl
+            // 
+            AdminLbl.AutoSize = true;
+            AdminLbl.Font = new Font("Segoe UI", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            AdminLbl.Location = new Point(31, 26);
+            AdminLbl.Name = "AdminLbl";
+            AdminLbl.Size = new Size(82, 31);
+            AdminLbl.TabIndex = 33;
+            AdminLbl.Text = "Admin";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(31, 63);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(829, 433);
+            dataGridView1.TabIndex = 31;
+            // 
+            // AdminPanel
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(891, 575);
+            Controls.Add(GenreCmbBox);
+            Controls.Add(label11);
+            Controls.Add(DeleteTxtBox);
+            Controls.Add(DeleteBookLbl);
+            Controls.Add(ResultTxtBox);
+            Controls.Add(AcadCmbBox);
+            Controls.Add(AcademicsLbl);
+            Controls.Add(NonFictionCmbBox);
+            Controls.Add(NonFinctionLbl);
+            Controls.Add(FictionCmbBox);
+            Controls.Add(FictionLbl);
+            Controls.Add(NextBttn);
+            Controls.Add(BackBttn);
+            Controls.Add(DeleteBttn);
+            Controls.Add(UpdateBttn);
+            Controls.Add(AddBttn);
+            Controls.Add(PublishTxtBox);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(ISBNTxtBox);
+            Controls.Add(label4);
+            Controls.Add(AuthorTxtBox);
+            Controls.Add(label3);
+            Controls.Add(TitleTxtBox);
+            Controls.Add(label5);
+            Controls.Add(AdminLbl);
+            Controls.Add(dataGridView1);
+            Name = "AdminPanel";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox GenreCmbBox;
+        private Label label11;
+        private TextBox DeleteTxtBox;
+        private Label DeleteBookLbl;
+        private TextBox ResultTxtBox;
+        private ComboBox AcadCmbBox;
+        private Label AcademicsLbl;
+        private ComboBox NonFictionCmbBox;
+        private Label NonFinctionLbl;
+        private ComboBox FictionCmbBox;
+        private Label FictionLbl;
+        private Button NextBttn;
+        private Button BackBttn;
+        private Button DeleteBttn;
+        private Button UpdateBttn;
+        private Button AddBttn;
+        private TextBox PublishTxtBox;
+        private Label label7;
+        private Label label6;
+        private TextBox ISBNTxtBox;
+        private Label label4;
+        private TextBox AuthorTxtBox;
+        private Label label3;
+        private TextBox TitleTxtBox;
+        private Label label5;
+        private Label AdminLbl;
+        private DataGridView dataGridView1;
+    }
+}
