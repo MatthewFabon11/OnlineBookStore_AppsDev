e212cb0d2332ac3a481df5dc30a3192186602276
Monilar-exe <addisonmonilar@gmail.com>
2024-11-29 18:16:22 +0800

    Add files via upload

--- OnlineBookStore_AppsDev/Program.cs
+++ OnlineBookStore_AppsDev/Program.cs
@@ -1,4 +1,4 @@
-namespace OnlineBookStore_AppsDev
+namespace OnlineBookStore
 {
     internal static class Program
     {
@@ -11,7 +11,7 @@ namespace OnlineBookStore_AppsDev
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new AdminPanel());
+            Application.Run(new Entry());
         }
     }
 }
